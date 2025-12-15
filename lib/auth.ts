2659c08988183c02590a9d6f664baa6394c2ee30
gabrielmayer32 <gabriel0mayer@gmail.com>
2025-12-15 15:02:58 +0400

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -70,5 +70,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
 });
